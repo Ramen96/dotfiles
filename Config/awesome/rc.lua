c9a51ed45ac21df1a0818b219889bc533b71a563
Ramen96 <eric.worsfold@gmail.com>
2024-05-27 11:32:52 -0400

    updated awesome config

--- Config/awesome/rc.lua
+++ Config/awesome/rc.lua
@@ -596,17 +596,21 @@ client.connect_signal("request::titlebars", function(c)
     local buttons = gears.table.join(
         awful.button({ }, 1, function()
             c:emit_signal("request::activate", "titlebar", {raise = true})
-            awful.mouse.client.move(c)
+            -- Comment out the line below to prevent window movement
+            -- awful.mouse.client.move(c)
         end),
         awful.button({ }, 3, function()
             c:emit_signal("request::activate", "titlebar", {raise = true})
-            awful.mouse.client.resize(c)
+            -- Comment out the line below to prevent window resizing
+            -- awful.mouse.client.resize(c)
         end)
     )
 
     awful.titlebar(c) : setup {
         { -- Left
-            awful.titlebar.widget.iconwidget(c),
+            awful.titlebar.widget.closebutton    (c),
+            awful.titlebar.widget.maximizedbutton(c),
+            awful.titlebar.widget.minimizebutton (c),
             buttons = buttons,
             layout  = wibox.layout.fixed.horizontal
         },
@@ -619,16 +623,12 @@ client.connect_signal("request::titlebars", function(c)
             layout  = wibox.layout.flex.horizontal
         },
         { -- Right
-            awful.titlebar.widget.floatingbutton (c),
-            awful.titlebar.widget.maximizedbutton(c),
-            awful.titlebar.widget.stickybutton   (c),
-            awful.titlebar.widget.ontopbutton    (c),
-            awful.titlebar.widget.closebutton    (c),
             layout = wibox.layout.fixed.horizontal()
         },
         layout = wibox.layout.align.horizontal
     }
 end)
+
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
--- Config/awesome/theme.lua
+++ Config/awesome/theme.lua
@@ -32,6 +32,22 @@ theme.taglist_bg_normal = "#000000"
 theme.taglist_fg_normal = "#aaaaaa"
 theme.taglist_font = "sans 12"
 
+-- Define paths to SVG icons
+local close_icon_path = "~/.config/awesome/icons/close.svg"
+local maximize_icon_path = "~/.config/awesome/icons/maximize.svg"
+local minimize_icon_path = "~/.config/awesome/icons/minimize.svg"
+
+-- Load SVG icons as Cairo surfaces
+local close_icon = gears.surface.load(close_icon_path)
+local maximize_icon = gears.surface.load(maximize_icon_path)
+local minimize_icon = gears.surface.load(minimize_icon_path)
+
+-- Add icons to the theme table
+theme.icons = {
+    close = close_icon,
+    maximize = maximize_icon,
+    minimize = minimize_icon
+}
 
 -- Return the theme table to be used by Awesome WM
 return theme
