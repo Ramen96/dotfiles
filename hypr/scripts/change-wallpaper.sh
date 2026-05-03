418349e1f110e60dfce7c5071677a58ebad18f58
Ramen96 <eric.worsfold@gmail.com>
2026-05-03 16:21:32 -0400

    deleted old script

--- hypr/scripts/change-wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Preload a new wallpaper
-hyprctl hyprpaper preload "~/.config/wallpapers/mountains.jpg"
-# Set it to monitor (e.g., eDP-1)
-hyprctl hyprpaper wallpaper "DP-6, ~/.config/wallpapers/mountains.jpg"
-# Unload unused images to save RAM
-hyprctl hyprpaper unload unused
